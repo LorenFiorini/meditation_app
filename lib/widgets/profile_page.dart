db8522f5c808eb723c93a4994c9415624855b3d3
Lorenzo Fiorini <lorenfiorini@gmail.com>
2022-03-23 16:42:08 +0100

    Create profile_page.dart

---
+++ lib/widgets/profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: const Color(0xff121421),
+      backgroundColor: Color(0xff000063),
+      body: Container(),
+    );
+  }
+}
