8ccd606fb7fbe58b0e1d53366495c179a9806487
Lorenzo Fiorini <lorenfiorini@gmail.com>
2022-03-23 16:42:32 +0100

    Create charts_page.dart

---
+++ lib/charts_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class ChartsPage extends StatelessWidget {
+  const ChartsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: const Color(0xff121421),
+      backgroundColor: Color(0xff000063),
+      body: Container(),
+    );
+  }
+}
